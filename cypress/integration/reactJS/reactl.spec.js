cbfcd8173a8165411515cc76758cc497ac85175e
sdetAutomation <sdet.testautomation@gmail.com>
2018-06-06 00:27:14 -0500

    Fix failing test, click first element

--- cypress/integration/reactJS/reactl.spec.js
+++ cypress/integration/reactJS/reactl.spec.js
@@ -8,14 +8,14 @@ describe('React Smoke Test ', function() {
   it("Nav to text fields and asserts", function() {
     cy.visit(Cypress.env('reactUrl')) // reading value from cypress.json
 
-    cy.get(btnGetStarted).click({ multiple: true });
+    cy.get(btnGetStarted).first().click();
 
     cy.url().should('include', '/hello-world.html')
 
     cy.get(txtHelloWorld)
       .should('have.text', 'Hello World')
     //nav back to home page
-    cy.get(imgReact).click({ multiple: true });
+    cy.get(imgReact).click();
     // verify button is on home page
     cy.get(btnGetStarted).should('be.visible')
 
