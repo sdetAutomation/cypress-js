5236ea5dda318d4060ef28c01eba139a75912f0f
sdetAutomation <sdet.testautomation@gmail.com>
2018-06-06 21:41:29 -0500

    remove wait

--- cypress/integration/reactJS/reactl.spec.js
+++ cypress/integration/reactJS/reactl.spec.js
@@ -1,4 +1,4 @@
- var btnGetStarted = '.css-1053yfl';
+ var btnGetStarted = '[href="/docs/hello-world.html"]';
  var txtHelloWorld = '.css-hgc6lu';
  var imgReact = '.css-184keb2';
 
@@ -7,17 +7,13 @@ describe('React Smoke Test ', function() {
 
   it("Clicks Get Started Button, then nav back to home page", function() {
     cy.visit(Cypress.env('reactUrl')) // reading value from cypress.json
-    cy.wait(10000)
     cy.get(btnGetStarted).first().click();
     cy.url().should('include', '/hello-world.html')
-    cy.wait(1000)
     cy.get(txtHelloWorld)
       .should('have.text', 'Hello World')
     //nav back to home page
-//    cy.get(imgReact).click();
-//    cy.wait(1000)
-//    // verify button is on home page
-//    cy.get(btnGetStarted).should('be.visible')
-
+    cy.get(imgReact).click();
+    // verify button is on home page
+    cy.get(btnGetStarted).should('be.visible')
   })
 })
